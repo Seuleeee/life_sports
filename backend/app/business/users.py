c4a7fd9e98fa24aaf889c00d41588aafdf567acf
Seuleeee <bryantjo1224@gmail.com>
2022-09-26 15:12:49 +0900

    fea) 회원가입 시 비밀번호 hasing

--- backend/app/business/users.py
+++ backend/app/business/users.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
+import bcrypt
 
 from app.schemas.users import UsersCreateItem, UsersUpdateItem
 from app.services.user_service import UserService
@@ -7,7 +8,15 @@ from app.services.user_service import UserService
 
 class UserBusiness:
     def create(self, db: Session, user: UsersCreateItem):
+        hashed_password: bytes = self.__hash_password(user.password)
+        user.password = hashed_password
         return UserService().create(db, user)
+
+    def __hash_password(self, plain_password: str):
+        bytes_password: bytes = plain_password.encode('utf-8')
+        salt: bytes = bcrypt.gensalt()
+        hashed_password: bytes = bcrypt.hashpw(bytes_password, salt)
+        return hashed_password
 
     def read_list(self, db: Session, skip: int = 0, limit: int = 100):
         return UserService().read_list(db, skip, limit)
@@ -23,3 +32,6 @@ class UserBusiness:
 
     def delete(self, db: Session, user_id: str):
         return UserService().delete(db, user_id)
+
+    def verify_password(self, input_password, stored_password) -> bool:
+        return bcrypt.checkpw(input_password, stored_password)

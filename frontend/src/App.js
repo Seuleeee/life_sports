4a990ab85fbb4f24f1ada6cd8006b3589a60fa14
Seuleeee <bryantjo1224@gmail.com>
2022-10-05 14:20:11 +0900

    FE: pages: 로그인 페이지 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 import { Home, Auth } from './pages';
 
-
-
 function App(props) {
-    const router = createBrowserRouter([
-        {
-            path: "/",
-            element: <Home />
-        },
-        {
-            path: "/auth",
-            element: <Auth />
-        },
-    ])
+	const router = createBrowserRouter([
+		{
+			path: '/',
+			element: <Home />,
+		},
+		{
+			path: '/auth',
+			element: <Auth />,
+		},
+	]);
 
-    return (
-        <RouterProvider router={router} />
-    )
+	return (
+		<div className={'auth-wrapper'}>
+			<div className={'auth-inner'}>
+				<RouterProvider router={router} />
+			</div>
+		</div>
+	);
 }
-
 
 export default App;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-
+import Login from '../components/Login';
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                Home
-            </div>
-        );
-    }
+	render() {
+		return <Login />;
+	}
 }
 
 export default Home;
